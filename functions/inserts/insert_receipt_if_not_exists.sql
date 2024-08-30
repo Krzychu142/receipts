cd2b54f97f901c69daaa95407df648218319a201
Krzysztof Radzięta <91392466+Krzychu142@users.noreply.github.com>
2024-08-30 13:33:57 +0000

    definded parameters

--- functions/inserts/insert_receipt_if_not_exists.sql
+++ functions/inserts/insert_receipt_if_not_exists.sql
@@ -1,8 +1,15 @@
-CREATE OR REPLACE FUNCTION insert_receipt_if_not_exists()
+CREATE OR REPLACE FUNCTION insert_receipt_if_not_exists(
+    p_store_id INT, 
+    P_currency_id INT, 
+    P_total NUMERIC(10,2),
+    p_receipt_date DATE, 
+    p_is_online BOOLEAN, 
+    receipt_scan TEXT
+)
 RETURNS record AS $$
 DECLARE
     v_receipt_row record;
 BEGIN
-    
+
 END;
 $$ LANGUAGE plpgsql;
