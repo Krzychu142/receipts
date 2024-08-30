051511c6c624a6adb2485009b1c999f7227b8e07
Krzysztof Radzięta <91392466+Krzychu142@users.noreply.github.com>
2024-08-30 06:26:35 +0000

    changed formatting style, daily data update

--- functions/selects/select_store_by_name_and_address.sql
+++ functions/selects/select_store_by_name_and_address.sql
@@ -1,10 +1,11 @@
 CREATE OR REPLACE FUNCTION select_store_by_name_and_address(p_name VARCHAR, p_address VARCHAR) RETURNS record AS $$
-DECLARE v_store_row record;
+DECLARE 
+    v_store_row record;
 BEGIN
-SELECT * INTO v_store_row
-FROM stores
-WHERE name = p_name
-    AND address = p_address;
-RETURN v_store_row;
+    SELECT * INTO v_store_row
+    FROM stores
+    WHERE name = p_name AND address = p_address;
+
+    RETURN v_store_row;
 END;
 $$ LANGUAGE plpgsql;
--- functions/updates/update_store_website.sql
+++ functions/updates/update_store_website.sql
@@ -1,13 +1,16 @@
 CREATE OR REPLACE FUNCTION update_store_website(p_store_id INTEGER, p_website VARCHAR) RETURNS record AS $$
-DECLARE v_store_row record;
+DECLARE 
+    v_store_row record;
 BEGIN
-UPDATE stores
-SET website = p_website
-WHERE store_id = p_store_id
-RETURNING * INTO v_store_row;
-IF v_store_row.store_id IS NULL THEN RAISE EXCEPTION 'Store with ID % not found',
-p_store_id;
-END IF;
-RETURN v_store_row;
+    UPDATE stores
+    SET website = p_website
+    WHERE store_id = p_store_id
+    RETURNING * INTO v_store_row;
+    
+    IF v_store_row.store_id IS NULL THEN 
+        RAISE EXCEPTION 'Store with ID % not found', p_store_id;
+    END IF;
+
+    RETURN v_store_row;
 END;
 $$ LANGUAGE plpgsql;
