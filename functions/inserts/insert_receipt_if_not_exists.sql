e1dd7aeca640c0397fee3886487884c30e449dd2
Krzysztof Radzięta <kustosz142@gmail.com>
2024-09-01 18:41:03 +0200

    start_working_on_insert_receipt

--- functions/inserts/insert_receipt_if_not_exists.sql
+++ functions/inserts/insert_receipt_if_not_exists.sql
@@ -1,5 +1,17 @@
-CREATE OR REPLACE FUNCTION insert_receipt_if_not_exists()
+CREATE OR REPLACE FUNCTION insert_receipt_if_not_exists(
+    p_store_id INT,
+    p_currency_id INT,
+    p_total NUMERIC(10, 2),
+    p_receipt_date DATE,
+    p_is_online BOOLEAN,
+    p_receipt_scan TEXT
+)
 RETURNS record AS $$
+DECLARE
+    v_receipt_row record;
 CREATE
+
+    v_receipt_row := select_receipt_by_unique_receipt();
+
 END;
 $$ LANGUAGE plpgsql;
