1ce0d1cf9cee120eb99e6ba70a384e4cc1eeb14d
Krzysztof Radzięta <kustosz142@gmail.com>
2024-09-01 21:49:13 +0200

    scratch of if not exists path

--- functions/inserts/insert_receipt_if_not_exists.sql
+++ functions/inserts/insert_receipt_if_not_exists.sql
@@ -9,6 +9,8 @@ CREATE OR REPLACE FUNCTION insert_receipt_if_not_exists(
 RETURNS record AS $$
 DECLARE
     v_receipt_row record;
+    v_store_row record;
+    v_currency_row record;
 CREATE
 
     v_receipt_row := select_receipt_by_unique_receipt(
@@ -21,12 +23,32 @@ CREATE
     );
 
     IF v_receipt_row IS NULL THEN
-      -- not exists
-      -- check is p_store_id valid object
-      -- if not raise error
-      -- check is p_currency_id valid object 
-      -- if not raise error
-      -- insert new object
+        v_store_row := select_store_by_id(p_store_id);
+        IF v_store_row IS NULL THEN 
+            RAISE EXCEPTION 'Store with ID % not found', p_store_id;
+        END IF;
+        v_currency_row := select_currency_by_id(p_currency_id);
+        IF v_currency_row IS NULL THEN
+            RAISE EXCEPTION 'Currency with ID % not found', p_currency_id;
+        END IF;
+
+        INSERT receipts (
+            store_id, 
+            currency_id, 
+            total,
+            receipt_date,
+            is_online,
+            receipt_scan
+        )
+        VALUES(
+            p_store_id, 
+            p_currency_id, 
+            p_total,
+            p_receipt_date,
+            p_is_online,
+            p_receipt_scan  
+        )
+        RETURNING * INTO v_receipt_row;
     ELSE
         -- exists
         -- is old receipt_date is null and new is not null
