2ebad56dc840d7a0c66d08ab339a3bfc84a125b1
Krzysztof Radzięta <kustosz142@gmail.com>
2024-09-03 21:11:31 +0200

    store inserted

--- functions/inserts/insert_tables_by_data_from_csv.sql
+++ functions/inserts/insert_tables_by_data_from_csv.sql
@@ -4,26 +4,25 @@ DECLARE
     record_data record;
     store_name VARCHAR(180);
     store_address VARCHAR(255);
-    website VARCHAR(255);
+    store_website VARCHAR(255);
+    v_store_row record;
 BEGIN
     FOR record_data IN 
         SELECT * FROM csv_plain_data
     LOOP
-        -- FROM EVERY RECORD WE NEED TO TRY TO CREATE:
-        -- store
-        -- currencie
-        -- category 
-        -- unit
-        -- product
-        -- receipt
-        -- purchase
+        BEGIN
 
-        -- LET TRY FROM RECEIPT - WE NEED STORE AND CURRENCIE, other data are ready to use from csv
+            store_name := record_data.sklep;
+            PERFORM validate_parameter_is_not_null(store_name, 'Store name');
+            store_address := COALESCE(record_data.adres, '');
+            store_website := record_data.strona_internetowa;
 
-        store_name := record_data.sklep;
-        store_address := record_data.adres;
-        store_website := record_data.strona_internetowa;
+            v_store_row := insert_store_if_not_exists(store_name, store_address, store_website);
 
+        EXCEPTION
+            WHEN OTHERS THEN
+                RAISE;
+        END;
     END LOOP;
 END;
 $$ LANGUAGE plpgsql;
