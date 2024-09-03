7b940fcdd39d620db8d41cde2019ee6c6908b7f3
Krzysztof Radzięta <91392466+Krzychu142@users.noreply.github.com>
2024-09-03 08:01:43 +0000

    start of blueprint of insert_tables

--- functions/inserts/insert_tables_by_data_from_csv.sql
+++ functions/inserts/insert_tables_by_data_from_csv.sql
@@ -6,6 +6,16 @@ BEGIN
     FOR record_data IN 
         SELECT * FROM csv_plain_data
     LOOP
+        -- FROM EVERY RECORD WE NEED TO TRY TO CREATE:
+        -- store
+        -- currencie
+        -- category 
+        -- unit
+        -- product
+        -- receipt
+        -- purchase
+
+        -- LET TRY FROM RECEIPT - WE NEED STORE AND CURRENCIE, other data are ready to use from csv
     END LOOP;
 END;
 $$ LANGUAGE plpgsql;
