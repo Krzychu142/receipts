6115fdc44cdc5c7aea93a54024d66ce245de781a
Krzysztof Radzięta <kustosz142@gmail.com>
2024-09-03 20:00:38 +0200

    typo correct

--- functions/helpers/validate_positive_number.sql
+++ functions/helpers/validate_positive_number.sql
@@ -1,18 +1,18 @@
 CREATE OR REPLACE FUNCTION validate_positive_number(
     p_number_to_validate NUMERIC,
-    p_name_of_validating_parametr TEXT,
-    p_is_zero_posibble BOOLEAN DEFAULT TRUE 
+    p_name_of_validating_parameter TEXT,
+    p_is_zero_possible BOOLEAN DEFAULT TRUE 
 )
 RETURNS VOID AS $$
 BEGIN
-    IF p_is_zero_posibble = FALSE AND p_number_to_validate <= 0 THEN
+    IF p_is_zero_possible = FALSE AND p_number_to_validate <= 0 THEN
         RAISE EXCEPTION 'Value of % cannot be less than or equal to 0. % = %', 
-                        p_name_of_validating_parametr, p_name_of_validating_parametr, p_number_to_validate;
+                        p_name_of_validating_parameter, p_name_of_validating_parameter, p_number_to_validate;
     END IF;
 
     IF p_number_to_validate < 0 THEN
         RAISE EXCEPTION 'Value of % cannot be less than 0. % = %', 
-                        p_name_of_validating_parametr, p_name_of_validating_parametr, p_number_to_validate;
+                        p_name_of_validating_parameter, p_name_of_validating_parameter, p_number_to_validate;
     END IF;
 END;
 $$ LANGUAGE plpgsql;
