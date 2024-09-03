84af6434392aaf9c251d50e5a846ba08af575c64
Krzysztof Radzięta <91392466+Krzychu142@users.noreply.github.com>
2024-09-03 11:25:19 +0000

    created helper function for validation is number lower or equal zero

--- functions/inserts/insert_tables_by_data_from_csv.sql
+++ functions/inserts/insert_tables_by_data_from_csv.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE FUNCTION insert_tables_by_data_from_csv()
-RETURNS void $$
-DECLARE
-    record_data record;
+RETURNS VOID AS $$
+-- DECLARE
+--     record_data record;
 BEGIN
     FOR record_data IN 
         SELECT * FROM csv_plain_data
