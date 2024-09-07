d7b67edef0cde841ada9241ff67f425284062767
Krzysztof Radzięta <kustosz142@gmail.com>
2024-09-07 14:02:27 +0200

    repaired validate is bool function, added cast to text

--- functions/inserts/insert_tables_by_data_from_csv.sql
+++ functions/inserts/insert_tables_by_data_from_csv.sql
@@ -13,6 +13,8 @@ DECLARE
     receipt_date_string TEXT;
     receipt_date_date DATE;
     receipt_is_online BOOLEAN;
+    receipt_scan TEXT;
+    v_receipt_row record;
 BEGIN
     BEGIN
         FOR record_data IN 
@@ -22,13 +24,11 @@ BEGIN
             PERFORM validate_parameter_is_not_null(store_name, 'Store name');
             store_address := COALESCE(record_data.adres, '');
             store_website := record_data.strona_internetowa;
-
             v_store_row := insert_store_if_not_exists(store_name, store_address, store_website);
 
             currency_code := record_data.waluta;
             PERFORM validate_parameter_is_not_null(currency_code, 'Currency code');
             currency_description := record_data.opis_waluty;
-
             v_currency_row := insert_currency_if_not_exists(currency_code, currency_description);
 
             receipt_total := record_data.suma;
@@ -37,7 +37,9 @@ BEGIN
             PERFORM validate_string_as_date(receipt_date_string);
             receipt_date_date := to_date(receipt_date_string, 'YYYY-MM-DD');
             receipt_is_online := record_data.czy_internetowy;
-            -- validate receipt_is_online is it bool type
+            PERFORM validate_parameter_is_boolean_type(receipt_is_online, 'Is receipt online');
+            -- receipt_scan := coalesce(record_data.skan_paragonu, NULL);
+            -- v_receipt_row := insert_receipt_if_not_exists(v_store_row.store_id, v_currency_row.currency_id, receipt_total, receipt_date_date, receipt_is_online, receipt_scan);
             
         END LOOP;
         
