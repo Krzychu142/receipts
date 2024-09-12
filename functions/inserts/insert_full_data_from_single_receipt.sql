080f57f2710a84ba6f5f75b67f9e5d1205be5e11
Krzysztof Radzięta <kustosz142@gmail.com>
2024-09-12 22:14:54 +0200

    working on testing receipt to purchase

--- functions/inserts/insert_full_data_from_single_receipt.sql
+++ functions/inserts/insert_full_data_from_single_receipt.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION insert_single_receipt(
+CREATE OR REPLACE FUNCTION insert_full_data_from_single_receipt(
     store_name VARCHAR(180),
     store_address VARCHAR(255),
     store_website VARCHAR(255),
@@ -8,20 +8,7 @@ CREATE OR REPLACE FUNCTION insert_single_receipt(
     receipt_date_string TEXT,
     receipt_is_online BOOLEAN,
     receipt_scan TEXT,
-    category_name VARCHAR(100),
-    unit_name VARCHAR(50),
-    products purchased_product[],
-    product_name VARCHAR(200),
-    product_link VARCHAR(255),
-    product_is_virtual BOOLEAN,
-    product_is_fee BOOLEAN,
-    product_description TEXT,
-    purchase_price NUMERIC(10, 2),
-    purchase_discount NUMERIC(10, 2),
-    purchase_quantity NUMERIC(10, 3),
-    purchase_is_warranty BOOLEAN,
-    purchase_warranty_expiration_string TEXT,
-    purchase_warranty_expiration_date DATE
+    products purchased_product[]
 )
 RETURNS VOID AS $$
 DECLARE
@@ -41,6 +28,7 @@ DECLARE
     v_product_link VARCHAR(255);
     v_product_is_virtual BOOLEAN;
     v_product_is_fee BOOLEAN;
+    v_product_description TEXT;
     v_product_row record;
     v_purchase_price NUMERIC(10, 2);
     v_purchase_discount NUMERIC(10, 2);
@@ -51,15 +39,15 @@ DECLARE
 BEGIN
     PERFORM validate_parameter_is_not_null(store_name, 'Store name');
     v_store_row := insert_store_if_not_exists(
-        store_name,
-        COALESCE(store_address, ''),
-        COALESCE(store_website, '')
+        lower(store_name),
+        COALESCE(lower(store_address), ''),
+        COALESCE(lower(store_website), '')
     );
 
     PERFORM validate_parameter_is_not_null(currency_code, 'Currency code');
     v_currency_row := insert_currency_if_not_exists(
-        currency_code,
-        COALESCE(currency_description, '')
+        lower(currency_code),
+        COALESCE(lower(currency_description), '')
     );
 
     PERFORM validate_positive_number(receipt_total, 'Receipt total', FALSE);
@@ -73,7 +61,7 @@ BEGIN
         receipt_total,
         v_receipt_date_date,
         receipt_is_online,
-        COALESCE(receipt_scan, '')
+        COALESCE(lower(receipt_scan), '')
     );
 
     FOREACH product_purchase_record IN ARRAY products
@@ -98,14 +86,14 @@ BEGIN
         END IF;
 
         v_unit_row := insert_unit_if_not_exists(
-            v_unit_name,
+            lower(v_unit_name),
             v_base_unit_row.unit_id,
             v_conversion_multiplier
         );
 
         v_category_name := product_purchase_record.category_name;
         PERFORM validate_parameter_is_not_null(v_category_name, 'Category name');
-        v_category_row := insert_category_if_not_exists(v_category_name);
+        v_category_row := insert_category_if_not_exists(lower(v_category_name));
 
         v_product_name := product_purchase_record.product_name;
         PERFORM validate_parameter_is_not_null(v_product_name, 'Product name');
@@ -115,11 +103,11 @@ BEGIN
         v_product_description := COALESCE(product_purchase_record.description, '');
 
         v_product_row := insert_product_if_not_exists(
-            v_product_name,
+            lower(v_product_name),
             v_category_row.category_id,
             v_product_link,
             v_product_is_virtual,
-            v_product_description
+            lower(v_product_description)
         );
 
         v_purchase_price := product_purchase_record.price;
@@ -130,7 +118,7 @@ BEGIN
         PERFORM validate_positive_number(v_purchase_quantity, 'Quantity', FALSE);
         v_purchase_is_warranty := COALESCE(product_purchase_record.is_warranty, FALSE);
         PERFORM validate_parameter_is_boolean_type(v_purchase_is_warranty, 'Is purchase on warranty');
-        v_purchase_warranty_expiration_date := product_purchase_record
+        v_purchase_warranty_expiration_date := product_purchase_record;
 
         IF
             (v_purchase_is_warranty IS TRUE AND v_purchase_warranty_expiration_date IS NULL)
