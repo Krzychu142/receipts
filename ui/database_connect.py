3035998d1ddfdf36266b7640b1bef7e4983ea0d4
Krzysztof Radzięta <kustosz142@gmail.com>
2024-09-20 19:36:15 +0200

    wip

--- ui/database_connect.py
+++ ui/database_connect.py
@@ -1,13 +1,11 @@
 import psycopg2
-from psycopg2 import sql
 import configparser
+import logging
 
-def database_connect():
+def get_db_connection():
     config = configparser.ConfigParser()
     config.read('config.ini')
-    
-    connection = None
-    
+
     try:
         connection = psycopg2.connect(
             dbname=config['postgresql']['dbname'],
@@ -16,12 +14,113 @@ def database_connect():
             host=config['postgresql']['host'],
             port=config['postgresql']['port']
         )
+        return connection
+    except psycopg2.Error as e:
+        logging.error(f"Failed to connect to the database: {e}")
+        return None
+
+def get_store_names_with_addresses():
+    connection = get_db_connection()
+    store_entries = []
+    if connection:
+        try:
+            cursor = connection.cursor()
+            cursor.execute("SELECT DISTINCT name, address FROM stores;")
+            stores = cursor.fetchall()
+            store_entries = [f"{store[0]} - {store[1]}" for store in stores]
+        except psycopg2.Error as e:
+            logging.error(f"Failed to fetch store names and addresses: {e}")
+        finally:
+            cursor.close()
+            connection.close()
+    return store_entries
+
+def get_categories():
+    connection = get_db_connection()
+    categories = []
+    if connection:
+        try:
+            cursor = connection.cursor()
+            cursor.execute("SELECT name FROM categories;")
+            cats = cursor.fetchall()
+            categories = [cat[0] for cat in cats]
+        except psycopg2.Error as e:
+            logging.error(f"Failed to fetch categories: {e}")
+        finally:
+            cursor.close()
+            connection.close()
+    return categories
+
+def get_units():
+    connection = get_db_connection()
+    units = []
+    if connection:
+        try:
+            cursor = connection.cursor()
+            cursor.execute("SELECT name FROM units;")
+            us = cursor.fetchall()
+            units = [unit[0] for unit in us]
+        except psycopg2.Error as e:
+            logging.error(f"Failed to fetch units: {e}")
+        finally:
+            cursor.close()
+            connection.close()
+    return units
+
+def insert_receipt(receipt):
+    connection = get_db_connection()
+    if not connection:
+        logging.error("No connection to the database.")
+        return False
+    
+    try:
         cursor = connection.cursor()
         
-        cursor.execute("SELECT version();")
-        db_version = cursor.fetchone()
-        print(f"Connected to PostgreSQL database, version: {db_version}")
+        purchased_products = [
+            (
+                p['nazwa'],
+                p['jednostka'],
+                p.get('base_unit_name'),          # base_unit_name
+                p.get('conversion_multiplier'),    # conversion_multiplier
+                p['kategoria'],
+                p['cena'],
+                p['rabat'],
+                p['ilość'],
+                p.get('opis_produktu', ''),
+                p['czy_na_gwarancji'],
+                p.get('data_gwarancji', ''),
+                False,                             # Additional fields if required
+                None
+            )
+            for p in receipt['produkty']
+        ]
         
-    except Exception as error:
-        print(f"Failed to connect to the database: {error}")
-    
+        cursor.execute("""
+            SELECT insert_full_data_from_single_receipt(
+                %s, %s, %s,
+                %s, %s,
+                %s, %s, %s, %s, %s
+            );
+        """, (
+            receipt['sklep'],
+            receipt['adres'],
+            receipt.get('strona_internetowa', ''),
+            receipt['waluta'],
+            receipt.get('currency_description', ''),  # If needed
+            receipt['suma'],
+            receipt['data-zakupów'],
+            receipt['czy_internetowy'],
+            receipt.get('skan_paragonu', ''),
+            purchased_products
+        ))
+        
+        connection.rollback() # test rollback
+        logging.info("Receipt data successfully saved to the database.")
+        return True
+    except psycopg2.Error as e:
+        connection.rollback()
+        logging.error(f"Failed to save receipt data: {e}")
+        return False
+    finally:
+        cursor.close()
+        connection.close()
--- ui/main.py
+++ ui/main.py
@@ -1,13 +1,20 @@
-# source venv/bin/activate
-# deactivate
-from database_connect import database_connect
 
-from prompt_toolkit import prompt
+from prompt_user import prompt_receipt_data
+from database_connect import insert_receipt
+import logging
+import json
 
 def main():
-    database_connect()
-    user_input = prompt('Prompt test: ')
-    print(f'user_input=, {user_input}!')
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+    receipt = prompt_receipt_data()
+    print("\n--- Entered Receipt Data ---")
+    # print(json.dumps(receipt, indent=4, ensure_ascii=False))
+    # success = insert_receipt(receipt)
+    
+    # if success:
+        # print("Receipt data has been successfully saved to the database.")
+    # else:
+        # print("An error occurred while saving receipt data.")
 
 if __name__ == '__main__':
     main()
